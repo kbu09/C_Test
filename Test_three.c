2bb1fc17452641ff30aa1ce39594f60ced7c6c7b
kb <kbu8460@gmail.com>
2024-08-21 07:18:56 +0530

    all

--- Test_three.c
+++ Test_three.c
@@ -298,6 +298,68 @@ int main()
     return 0;
 }
 
+9] Implement a C program that multiplies two matrices.
+Ans =
+#include <stdio.h>
+int main()
+{
+    int r1, c1, r2, c2;
+
+    // Input dimensions for the matrices
+    printf("Enter rows and columns for the first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns for the second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    // Check if multiplication is possible
+    if (c1 != r2)
+    {
+        printf("Matrix multiplication not possible.\n");
+        return 1;
+    }
+    int matrix1[r1][c1], matrix2[r2][c2], result[r1][c2];
+    // Input elements for the first matrix
+    printf("Enter elements of the first matrix:\n");
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c1; j++)
+        {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+    printf("Enter elements of the second matrix:\n");
+    for (int i = 0; i < r2; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            result[i][j] = 0;
+            for (int k = 0; k < c1; k++)
+            {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+    printf("Resultant matrix:\n");
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
 10] Write a C program to count the frequency of each character in a given string.
 Ans =
 #include <stdio.h>
